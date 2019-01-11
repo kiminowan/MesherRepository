c0a57acff2783c7d0d053004eb2ff87bd774e57d
1910702226 <45481377+1910702226@users.noreply.github.com>
2019-01-11 16:19:53 +0800

    1

---
+++ Mesher/Mesher/Mesher.MVC/Controllers/AnalyzeEchartsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mesher.MVC.Controllers
+{
+    public class AnalyzeEchartsController : Controller
+    {
+        // GET: AnalyzeEcharts
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
